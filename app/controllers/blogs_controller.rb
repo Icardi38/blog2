57efc94955b0db3d9d815574bf06cf4d8364aa9c
Icardi38 <vm26190@hotmail.fr>
2018-02-02 15:05:53 +0100

    création bouton submit
    + changer le nom sur la bar de navigation : index => blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,9 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
+  def create
+    Blog.create title: params[:title] , content: params[:content]
+    redirect_to "blogs"
+  end
 end
